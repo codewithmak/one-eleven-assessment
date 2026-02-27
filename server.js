c2f1e3ab48d134b605414faf3efb5a47545dee31
Muthelo Kutama <muthelokutama4@gmail.com>
2026-02-27 09:38:32 +0200

    Initial commit for One Eleven task

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.post('/api/sort', (req, res) => {
+    const { data } = req.body;
+
+    
+    if (!data || typeof data !== 'string') {
+        return res.status(400).json({ error: 'Please send a valid string in the "data" field.' });
+    }
+
+    const sortedArray = data.toLowerCase().split('').sort();
+
+    return res.json({ word: sortedArray });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
